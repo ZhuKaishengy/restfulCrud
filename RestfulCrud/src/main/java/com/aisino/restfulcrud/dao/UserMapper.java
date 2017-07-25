11fc0918ac0ff159e24ca34036ef397328960019
ZhuKaishengy <13109894018@163>
2017-07-25 14:44:57 +0800

    搭建项目

---
+++ RestfulCrud/src/main/java/com/aisino/restfulcrud/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.aisino.restfulcrud.dao;
+
+import com.aisino.restfulcrud.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
